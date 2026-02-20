1fd7581071a00e72e373f7fb22c8c2daf2126e86
Muneeb Ahmed <muneebahmad25211@gmail.com>
2026-02-21 01:25:59 +0500

    Setup File added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Muneeb Ahmed',
+    author_email= 'muneebahmad25211@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
